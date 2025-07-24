410e3b80a158cdb625263e6267881e9591818afa
AngelMoZa <angelmorazazo@gmail.com>
2025-07-24 18:41:18 +0200

    firs commit

---
+++ src/test/java/com/masons/lenin/LeninApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masons.lenin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeninApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
